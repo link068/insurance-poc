d1fe6e0ad05cc196eac35f270c25f16a686de240
choang91 <choang@wisc.edu>
2017-07-07 21:32:08 -0700

    Auto-populate customer name and email in new application

--- Insurance2/src/main/webapp/scripts/controllers/newApplication.js
+++ Insurance2/src/main/webapp/scripts/controllers/newApplication.js
@@ -8,6 +8,11 @@
  */
 angular.module('sbAdminApp')
   .controller('NewApplicationCtrl', function($scope,$http,$window,NewApplicationFactory) {
+  	
+  	// Populate saved data from customer login
+  	$scope.email = sessionStorage.getItem('email');
+  	$scope.name = sessionStorage.getItem('name');
+  	
 	  $scope.submitInfo = function(){ 
 		  console.log("####!!!!!######$$$$$$$$$$$$$ =! ");
 		  var regData = new NewApplicationForm($scope.name,$scope.email,$scope.mobile,$scope.address,$scope.ssn,$scope.dob,$scope.occupation,$scope.salary,$scope.education, $scope.policy);
